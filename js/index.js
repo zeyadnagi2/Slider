0a2e8bd57292f47646f8e6d1f12029a4d656a414
Ziad El-Khashab <93553215+zeyadnagi2@users.noreply.github.com>
2025-06-24 19:40:16 +0300

    finish

--- js/index.js
+++ js/index.js
@@ -15,33 +15,42 @@ for (let i = 0; i < myItem.length; i++) {
 }
 
 closeBtn.nextElementSibling.addEventListener("click", function () {
-  slideNext();
+  slide(1);
 });
 
 closeBtn.previousElementSibling.addEventListener("click", function () {
-  slidePrevious();
+  slide(-1);
 });
 
-function slideNext() {
-  temp++;
-  if (temp < myItem.length) {
-    var imgsrc = myItem[temp].querySelector("img").getAttribute("src");
-    box.style.setProperty("background-image", `url(${imgsrc})`);
-  } else {
-    temp = 0;
-    var imgsrc = myItem[temp].querySelector("img").getAttribute("src");
-    box.style.setProperty("background-image", `url(${imgsrc})`);
+boxModel.addEventListener("click", function (e) {
+  if (e.target == e.currentTarget || e.target == closeBtn) {
+    closeModel();
   }
+});
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key == "ArrowRight") {
+    slide(1);
+  } else if (e.key == "ArrowLeft") {
+    slide(-1);
+  } else if (e.key == "Escape") {
+    closeModel();
+  }
+});
+
+function slide(step) {
+  temp = temp + step;
+  if (temp >= myItem.length) {
+    temp = 0;
+  } else if (temp < 0) {
+    temp = myItem.length - 1;
+  }
+  var imgsrc = myItem[temp].querySelector("img").getAttribute("src");
+  box.style.setProperty("background-image", `url(${imgsrc})`);
 }
 
-function slidePrevious() {
-  temp--;
-  if (temp < 0) {
-    temp = myItem.length - 1;
-    var imgsrc = myItem[temp].querySelector("img").getAttribute("src");
-    box.style.setProperty("background-image", `url(${imgsrc})`);
-  } else {
-    var imgsrc = myItem[temp].querySelector("img").getAttribute("src");
-    box.style.setProperty("background-image", `url(${imgsrc})`);
-  }
+function closeModel() {
+  boxModel.classList.add("d-none");
 }
